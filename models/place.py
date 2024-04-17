8f82d97b69a7d250e09047d85b6f9c624fc904a8
Arafa Khalaf <khalafarafa@gmail.com>
2024-04-17 14:28:10 -0700

    task 9, user+place+review

--- models/place.py
+++ models/place.py
@@ -1,64 +1,47 @@
 #!/usr/bin/python3
-"""Module for defining the Place class in the HBNB project"""
-
-import models
+"""
+Module containing the Place class to represent places
+"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
+import models
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from os import environ
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+if storage_engine == "db":
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id',
-                                 String(60),
+                          Column('place_id', String(60),
                                  ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id',
-                                 String(60),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
                                  ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False))
-                                 
+                                 primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
-    """Represents a place object."""
-    
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'places'
-        city_id = Column(String(60),
-                         ForeignKey("cities.id"),
-                         nullable=False)
-        user_id = Column(String(60),
-                         ForeignKey('users.id'),
-                         nullable=False)
-        name = Column(String(128),
-                      nullable=False)
-        description = Column(String(1024),
-                             nullable=True)
-        number_rooms = Column(Integer,
-                              default=0,
-                              nullable=False)
-    
-        number_bathrooms = Column(Integer,
-                                  default=0,
-                                  nullable=False)
-        max_guest = Column(Integer,
-                           default=0,
-                           nullable=False)
-        price_by_night = Column(Integer,
-                                default=0,
-                                nullable=False)
+    """
+    Place class representing places
+    """
+    __tablename__ = "places"
+    if storage_engine == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", cascade="all, delete",
-                               backref="places")
+        amenity_ids = []
+        reviews = relationship("Review", back_populates="place")
         amenities = relationship("Amenity",
-                                 secondary='place_amenity',
-                                 viewonly=False,
-                                 backref="place_amenities")
-
+                                 secondary=place_amenity,
+                                 back_populates="place_amenities",
+                                 viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -72,27 +55,29 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a place instance."""
-        super().__init__(*args, **kwargs)
+        @property
+        def reviews(self):
+            """Getter function for reviews attribute"""
+            result = []
+            temp = models.dummy_classes['Review']
+            for r in models.storage.all(temp).values():
+                if r.place_id == self.id:
+                    result.append(r)
+            return result
 
-    @property
-    def reviews(self):
-        """Attribute that returns a list of review instances."""
-        values_review = models.storage.all("Review").values()
-        list_review = []
-        for review in values_review:
-            if review.place_id == self.id:
-                list_review.append(review)
-        return list_review
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def amenities(self):
-            """Attribute that returns a list of Amenity instances."""
-            values_amenity = models.storage.all("Amenity").values()
-            list_amenity = []
-            for amenity in values_amenity:
-                if amenity.place_id == self.id:
-                    list_amenity.append(amenity)
-            return list_amenity
+            """Getter function for amenity attribute"""
+            result = []
+            temp = models.dummy_classes['Amenity']
+            for a in models.storage.all(temp).values():
+                if a in self.amenity_ids:
+                    result.append(a)
+            return result
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter for amenities class"""
+            temp = models.dummy_classes['Amenity']
+            if isinstance(obj, models.storage.all(temp)):
+                self.amenity_ids.append(obj.id)
--- models/user.py
+++ models/user.py
@@ -1,50 +1,29 @@
 #!/usr/bin/python3
-"""Module for defining the User class in the HBNB project"""
-
-import hashlib
-from os import getenv
-import models
+"""
+Module containing the User class
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from sqlalchemy import create_engine, Column, Integer, String
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
-    """Represents a user object."""
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        hashed_password = Column('password',
-                                 String(128),
-                                 nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
+    """
+    User class representing a user
+    """
+    if storage_engine == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
-        hashed_password = ""
+        password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a user instance."""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        """Getter method for password."""
-        return self.hashed_password
-
-    @password.setter
-    def password(self, pwd):
-        """Setter method for password."""
-        self.hashed_password = pwd
